3af5208c05042945dffc5de202ee0ddd78c96a58
alicelee <alice.lee@ga.gov.au>
2016-12-04 11:35:34 +1100

    Post cost line item metrics into ElasticSearch

--- additional-lambdas/cost_metrics/cost_metrics.js
+++ additional-lambdas/cost_metrics/cost_metrics.js
@@ -1,11 +1,14 @@
 var AWS = require('aws-sdk');
 var https = require('https');
 var crypto = require('crypto');
+var totalSearchStr = "Total for linked account";
+var lineSearchStr = "LinkedLineItem";
 
-/* Add comments
+
+/* Get access to GA consolidated bill account where the cost line item file stored  
  */
 exports.handler = function(input, context) {
-    var latestobj = [];
+    var latestObj = [];
     var elk_endpoint = new AWS.Endpoint(input.endpoint);
     var bucket = input.bucket;
     AWS.config.region = input.region;
@@ -38,7 +41,7 @@ exports.handler = function(input, context) {
     });
 
 /*
- * Find file and get metrics from s3 file
+ * Find the last modified cost line item file and get metrics from s3 file.  Generate JSON output and then post the metrics to ElasticSearch
  */
 function find_file_in_s3(s3) {
 
@@ -50,60 +53,68 @@ function find_file_in_s3(s3) {
           for (var i = 0; i < data.Contents.length; i++) {
             var name = data.Contents[i].Key;
             if (re.test(name)) {
-                if (latestobj.length === 0) { latestobj.push(data.Contents[i]); }
+                if (latestObj.length === 0) { latestObj.push(data.Contents[i]); }
                 else {
-                if (new Date(latestobj[0].LastModified).getTime() < new Date(data.Contents[i].LastModified).getTime()){
-                    latestobj.pop();
-                    latestobj.push(data.Contents[i]);
-                    console.log("latest: " + JSON.stringify(latestobj));
+                if (new Date(latestObj[0].LastModified).getTime() < new Date(data.Contents[i].LastModified).getTime()){
+                    latestObj.pop();
+                    latestObj.push(data.Contents[i]);
+                    // console.log("latest: " + JSON.stringify(latestObj));
                 }
             }
         }
     }
 
 
-    s3.getObject({Bucket: bucket, Key: latestobj[0].Key}, function(err, data) {
-        var searchstr = "Total for linked account";
-
+    s3.getObject({Bucket: bucket, Key: latestObj[0].Key}, function(err, data) {
         if (err) {
             console.log("Error getting object " + err);
             }
         else {
-            var arrout = [];
-            var matchitem = [];
-            var timestamp = new Date();
+            var arrOut = [];
+            var matchItem = [];
+            var timeStamp = new Date();
             var json_data = ' ';
-            arrout = String(data.Body).split('\n');
+            arrOut = String(data.Body).split('\n');
 
             //  index name format: cwl-YYYY.MM.DD
             var indexName = [
-                'cost-' + timestamp.getUTCFullYear(),              // year
-                ('0' + (timestamp.getUTCMonth() + 1)).slice(-2),  // month
-                ('0' + timestamp.getUTCDate()).slice(-2)          // day
+                'cost-' + timeStamp.getUTCFullYear(),              // year
+                ('0' + (timeStamp.getUTCMonth() + 1)).slice(-2),  // month
+                ('0' + timeStamp.getUTCDate()).slice(-2)          // day
             ].join('.');
 
             var action = { "index": {} };
             action.index._index = indexName;
-            action.index._type = "TotalCost";
+            action.index._type = "Cost";
 
-            for (var i = 0; i < arrout.length; i++) {
-                if (arrout[i].indexOf(searchstr) > -1) {
-                    matchitem = String(arrout[i]).substring(arrout[i].indexOf(searchstr)).split(',');
-                    var acitem = [];
-                    acitem.push({
-                        "AccountName": matchitem[0].substring(40, matchitem[0].length-2),
-                        "timestamp": new Date().toISOString(),
-                        "AccountId": matchitem[0].substring(26, 38),
-                        "TotalCost": parseFloat(matchitem[matchitem.length-1].replace(/"/g, '').replace(/\\/g, ''))
-                    });
-                json_data += [
-                    JSON.stringify(action),
-                    JSON.stringify(acitem),
-                    ].join('\n') + '\n';
-                    }
+            for (var i = 0; i < arrOut.length; i++) {
+		var acItem = [];
+		if (arrOut[i].indexOf(totalSearchStr) > -1) {
+	           matchItem = String(arrOut[i]).substring(arrOut[i].indexOf(totalSearchStr)).split(',');
+	           acItem.push({
+		      "AccountName": matchItem[0].substring(40, matchItem[0].length-2),
+		      "timestamp": new Date().toISOString(),
+		      "AccountId": matchItem[0].substring(26, 38),
+                      "TotalCost": parseFloat(matchItem[matchItem.length-1].replace(/"/g, '').replace(/\\/g, ''))
+		   });
+	         } else if (arrOut[i].indexOf(lineSearchStr) > -1) {
+			    matchItem = String(arrOut[i]).split('","');
+			    acItem.push({
+				"AccountName": matchItem[9],
+				"timestamp": new Date().toISOString(),
+				"AccountId": matchItem[2],
+				"ProductCode": matchItem[12],
+				"ProductName": matchItem[13],
+				"UsageType": matchItem[15],
+				"LineItemCost": parseFloat(matchItem[matchItem.length-1].replace(/"/g, '').replace(/\\/g, ''))
+		            });
+		 }
+		 if (acItem.length > 0) {
+			json_data += [ JSON.stringify(action), JSON.stringify(acItem), ].join('\n') + '\n';
+	         } 
                 }
-                json_data = json_data.replace(/\[/g, '').replace(/\]/g, '');
-                console.log(json_data);
+                json_data = json_data.replace(/\[/g, '').replace(/\]/g, '').replace(/\\/g, '');
+                console.log("JSON OUTPUT " + json_data);
                 postToES(json_data, elk_endpoint, AWS.config.region, context);
               }
           });
@@ -136,11 +147,11 @@ function postToES(json_str, endpoint, region, context) {
             respBody += chunk;
         });
         httpResp.on('end', function (chunk) {
-            console.log('Response: ' + respBody);
+            //console.log('Response: ' + respBody);
             context.succeed('sent json: ' + json_str);
         });
     }, function(err) {
-        console.log('Error: ' + err);
+        //console.log('Error: ' + err);
         context.fail('failed with error ' + err);
     });
     }
